9c50c185502ef2c894cec860da39d09debf31684
Paulo Cesar <paulinho4k10@gmail.com>
2022-07-18 00:20:26 -0300

    desafio_inverterArray

---
+++ Inverter Array/README.md
@@ -0,0 +1,3 @@
+Crie uma função que inverta a ordem de um array.
+
+Exemplo : [1,3,4,5] => [5,4,3,1]
---
+++ Inverter Array/index.js
@@ -0,0 +1,13 @@
+
+function inverter (array){
+
+const inverterArray= []
+
+  for (let i = array.length -1; i >= 0; i--){
+    inverterArray.push(array[i])
+  }
+  console.log(inverterArray)
+}
+
+inverter([10,10,20,20,30])
+inverter(["a","b", "d", "e"])
